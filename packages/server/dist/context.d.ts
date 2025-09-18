d7bd52fdc7b46106ddfa5829e0ed110751e8dad2
Andy Miller <rhuk@mac.com>
2025-09-17 23:13:12 -0600

    released to NPM
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- packages/server/dist/context.d.ts
+++
@@ -1,9 +0,0 @@
-import type { CommandName, CommandPayload, CommandResult, ToolResponse } from "@yetidevworks/shared";
-import { ExtensionBridge } from "./bridge.js";
-export declare class ExtensionContext {
-    private readonly bridge;
-    constructor(bridge: ExtensionBridge);
-    call<K extends CommandName>(command: K, payload?: CommandPayload<K> | undefined): Promise<CommandResult<K>>;
-    captureSnapshot(statusMessage?: string): Promise<ToolResponse>;
-}
-//# sourceMappingURL=context.d.ts.map
