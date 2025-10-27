fa3a4877a1f4a9fea39c55ba841dde4f81793ba6
Andy Miller <rhuk@mac.com>
2025-10-26 21:46:09 -0600

    remove dist files
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- packages/server/dist/server.d.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from "@modelcontextprotocol/sdk/server/index.js";
-import { ExtensionBridge } from "./bridge.js";
-export interface CreateMcpServerOptions {
-    name: string;
-    version: string;
-    bridge: ExtensionBridge;
-}
-export declare function createMcpServer(options: CreateMcpServerOptions): Promise<Server>;
-//# sourceMappingURL=server.d.ts.map
